6b53c902a944cdda4d8ef98a83c236f2e5c54187
Sergio Luis Para <sergio.l.para@gmail.com>
2020-05-30 17:22:53 +0200

    Minor changes to the EngineClock

--- src/Rulo/Rulo.Engine/Engine/EngineClock.cs
+++ src/Rulo/Rulo.Engine/Engine/EngineClock.cs
@@ -1,24 +1,84 @@
 ﻿using System;
 namespace Rulo.Engine.Engine
 {
+    internal interface IInternalEngineClock
+    {
+        void Update();
+    }
+
     public interface IEngineClock
     {
         DateTime Now();
         DateTime UtcNow();
     }
 
-    public class EngineClock : IEngineClock
+    public class TestEngineClock : IEngineClock, IInternalEngineClock
     {
+        public void Set(DateTime now)
+        {
+            mCurrentLocalDateTime = now.ToLocalTime();
+            mCurrentUtcDateTime = now.ToUniversalTime();
+        }
+
         DateTime IEngineClock.Now()
         {
-            return DateTime.Now;
+            return mCurrentLocalDateTime;
         }
 
         DateTime IEngineClock.UtcNow()
         {
-            return DateTime.UtcNow;
+            return mCurrentUtcDateTime;
         }
 
-        public static readonly IEngineClock Default = new EngineClock();
+        void IInternalEngineClock.Update()
+        {
+            // Nothing to do
+        }
+
+        DateTime mCurrentLocalDateTime;
+        DateTime mCurrentUtcDateTime;
+    }
+
+    public class DefaultEngineClock : IEngineClock, IInternalEngineClock
+    {
+        void IInternalEngineClock.Update()
+        {
+            DateTime now = DateTime.Now;
+            mCurrentLocalDateTime = now.ToLocalTime();
+            mCurrentUtcDateTime = now.ToUniversalTime();
+        }
+
+        DateTime IEngineClock.Now()
+        {
+            return mCurrentLocalDateTime;
+        }
+
+        DateTime IEngineClock.UtcNow()
+        {
+            return mCurrentUtcDateTime;
+        }
+
+        DateTime mCurrentLocalDateTime;
+        DateTime mCurrentUtcDateTime;
+    }
+
+    public static class EngineClock
+    {
+        public static IEngineClock Default
+        {
+            get
+            {
+                if (_Default == null)
+                    _Default = new DefaultEngineClock();
+
+                return _Default;
+            }
+            set
+            {
+                _Default = value;
+            }
+        }
+
+        static IEngineClock _Default;
     }
 }
