db0a685d6b6d04e7f125db75f7066d3cdd29ff3a
Sergio Luis Para <sergio.l.para@gmail.com>
2020-05-30 17:25:37 +0200

    Refactor the Result class to support the base Fact class

--- src/Rulo/Rulo.Engine/Engine/Facts/FactSource.cs
+++ src/Rulo/Rulo.Engine/Engine/Facts/FactSource.cs
@@ -3,25 +3,38 @@ using System.Threading.Tasks;
 
 namespace Rulo.Engine.Facts
 {
-    public class Result<T>
+    public abstract class Result
     {
-        public T Data { get; private set; }
         public TimeSpan TimeToLive { get; private set; }
 
-        public Result(T data, TimeSpan timeToLive)
+        public Result(TimeSpan timeToLive)
+        {
+            TimeToLive = timeToLive;
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        public T Data { get; private set; }
+
+        public Result(T data, TimeSpan timeToLive) : base(timeToLive)
         {
             Data = data;
-            TimeToLive = timeToLive;
         }
     }
 
     public abstract class FactSource
     {
-
+        internal abstract Task<Result> GetFact();
     }
 
     public abstract class FactSource<T> : FactSource
     {
-        public abstract Task<Result<T>> GetFact();
+        internal override async Task<Result> GetFact()
+        {
+            return await GetFactResult();
+        }
+
+        public abstract Task<Result<T>> GetFactResult();
     }
 }
