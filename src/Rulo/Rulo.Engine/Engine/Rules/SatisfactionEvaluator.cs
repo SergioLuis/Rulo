b1fe842ea34863f28c3bce08609e6d658c25b99d
Sergio Luis Para <sergio.l.para@gmail.com>
2020-05-31 23:13:05 +0200

    The SatisfactionEvaluator now fires rules
    I know that doesn't make any sense - I'll have to move this code around!

--- src/Rulo/Rulo.Engine/Engine/Rules/SatisfactionEvaluator.cs
+++ src/Rulo/Rulo.Engine/Engine/Rules/SatisfactionEvaluator.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 using Rulo.Engine.Conditions;
 using Rulo.Engine.Facts;
+using Rulo.Engine.Rules.Attributes;
 
-namespace Rulo.Engine.Engine.Rules
+namespace Rulo.Engine.Rules
 {
     public class SatisfactionEvaluator
     {
@@ -16,9 +18,86 @@ namespace Rulo.Engine.Engine.Rules
             mFactContainer = new FactContainer();
         }
 
+        public async Task<RuleEvaluationResult> EvaluateRule(IRule rule)
+        {
+            SatisfactionStatus ruleConditionSatisfaction =
+                await EvaluateCondition(rule.Condition);
+
+            if (!IsSatisfied(ruleConditionSatisfaction))
+                return RuleEvaluationResult.NotEvaluated;
+
+            Type ruleType = rule.GetType();
+            MethodInfo notAsyncFireMethodInfo = ruleType.GetMethod("Fire");
+            MethodInfo asyncFireMethodInfo = ruleType.GetMethod("FireAsync");
+
+            if (notAsyncFireMethodInfo == null && asyncFireMethodInfo == null)
+            {
+                throw new Exception(
+                    $"{ruleType} does not implement a 'Fire' or 'FireAsync' method!");
+            }
+
+            if (notAsyncFireMethodInfo != null && asyncFireMethodInfo != null)
+            {
+                throw new Exception(
+                    $"{ruleType} cannot implement both 'Fire' and 'FireAsync' at the same time!");
+            }
+
+            (MethodInfo fireMethodInfo, bool bIsAsync) =
+                notAsyncFireMethodInfo == null
+                    ? (asyncFireMethodInfo, true)
+                    : (notAsyncFireMethodInfo, false);
+
+            Type returnType = bIsAsync
+                ? typeof(Task<RuleEvaluationResult>)
+                : typeof(RuleEvaluationResult);
+
+            if (!fireMethodInfo.ReturnType.Equals(returnType))
+            {
+                throw new Exception(
+                    $"Method '{fireMethodInfo}' should return '{returnType}'!");
+            }
+
+            ParameterInfo[] fireMethodParameters = 
+                notAsyncFireMethodInfo.GetParameters();
+
+            Type factParamAttrType = typeof(FactParam);
+            List<string> requiredFactIds = fireMethodParameters
+                .Select(p => p.GetCustomAttribute(factParamAttrType) as FactParam)
+                .Select(attr => attr.FactId)
+                .ToList();
+
+            List<Task<Fact>> requestFactTasks = requiredFactIds
+                .Where(fact => !mFactContainer.IsCached(fact, EngineClock.Default.Now()))
+                .Select(fact => mFactSourceContainer.RequestNonGenericFact(fact))
+                .ToList();
+
+            await Task.WhenAll(requestFactTasks);
+
+            mFactContainer.AddFactRange(requestFactTasks.Select(t => t.Result));
+
+            List<Fact> factsForInvocation = requiredFactIds
+                .Select(fact => mFactContainer.PullFact(fact))
+                .ToList();
+
+            object[] objectsForInvocation = factsForInvocation
+                .Select(f => f.GetType().GetProperty("Data").GetValue(f))
+                .ToArray();
+
+            if (bIsAsync)
+            {
+                Task<RuleEvaluationResult> resultTask =
+                    fireMethodInfo.Invoke(rule, objectsForInvocation) as Task<RuleEvaluationResult>;
+                return await resultTask;
+            }
+
+            RuleEvaluationResult? result =
+                fireMethodInfo.Invoke(rule, objectsForInvocation) as RuleEvaluationResult?;
+
+            return result.Value;
+        }
+
         public async Task<SatisfactionStatus> EvaluateCondition(Condition condition)
         {
-            List<Task<Fact>> tasks = new List<Task<Fact>>();
             IEnumerable<string> requiredFactIds = condition.GetRequiredFactIds();
 
             List<Task<Fact>> requestFactTasks = requiredFactIds
@@ -26,13 +105,13 @@ namespace Rulo.Engine.Engine.Rules
                 .Select(fact => mFactSourceContainer.RequestNonGenericFact(fact))
                 .ToList();
 
-            await Task.WhenAll(tasks);
+            await Task.WhenAll(requestFactTasks);
 
             List<Fact> generatedFacts = requestFactTasks
                 .Select(task => task.Result)
                 .ToList();
 
-            mFactContainer.AddFactRange(generatedFacts);
+            mFactContainer.AddFactRange(requestFactTasks.Select(t => t.Result));
 
             SatisfactionStatus result = SatisfactionStatus.Unknown;
             using (EvaluationContext ctx = condition.StartEvaluation(mFactContainer))
@@ -40,12 +119,15 @@ namespace Rulo.Engine.Engine.Rules
                 result = await ctx.Evaluate();
             }
 
-            if ((result & SatisfactionStatus.Unknown) != SatisfactionStatus.Unknown)
+            if (result != SatisfactionStatus.Unknown)
                 return result;
 
             throw new Exception(
                 $"Got a SatisfactionStatus.Unknown after finishing evaluation!");
         }
+
+        static bool IsSatisfied(SatisfactionStatus status)
+            => (status & SatisfactionStatus.Satisfied) == SatisfactionStatus.Satisfied;
 
         readonly FactSourceContainer mFactSourceContainer;
         readonly FactContainer mFactContainer;
